faa1acabd9e22afaa686f889bb24b7c751202fc5
saisanthosh2218 <saisanthoshbunni@gmail.com>
2023-12-25 12:03:15 +0530

    first commit

---
+++ src/actions/cartActions.js
@@ -0,0 +1,27 @@
+export const addToCart = (foodItem) => {
+  return {
+    type: "ADD_TO_CART",
+    payload: foodItem,
+  };
+};
+
+export const removeFromCart = (itemId) => {
+  return {
+    type: "REMOVE_FROM_CART",
+    payload: itemId,
+  };
+};
+
+export const incrementItem = (itemId) => {
+  return {
+    type: "INCREMENT_ITEM",
+    payload: itemId,
+  };
+};
+
+export const decrementItem = (itemId) => {
+  return {
+    type: "DECREMENT_ITEM",
+    payload: itemId,
+  };
+};
